901c94fee9c56a3de688c9f91cc15414143daf7a
Nickola Jerjous <nickolajarjous333@gmail.com>
2025-08-16 11:22:58 +0300

    fix

--- android/src/main/kotlin/com/uhg0/ar_flutter_plugin_2/ArView.kt
+++ android/src/main/kotlin/com/uhg0/ar_flutter_plugin_2/ArView.kt
@@ -500,7 +500,7 @@ class ArView(
                         if (node != null) {
                             var anchorName: String? = null
                             var currentNode: Node? = node
-                            val nodeName = node.name;
+                            val nodeName = node.name
                             while (currentNode != null) {
                                 anchorNodesMap.forEach { (name, anchorNode) ->
                                     if (currentNode == anchorNode) {
